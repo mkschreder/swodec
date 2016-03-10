55c620e711d206e5c192d9973c3ec39fae7b5394
Marc Schink <swo-dev@marcschink.de>
2016-03-10 10:23:38 +0100

    Use __USE_MINGW_ANSI_STDIO for MinGW build
    This enables C99 compatible stdio functions on MinGW instead of using
    the incompatible functions provided by Microsoft.

--- src/main.c
+++ src/main.c
@@ -17,13 +17,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <glib.h>
 
-#include "config.h"
 #include "swodec.h"
 
 #define BUFFER_SIZE	1024
